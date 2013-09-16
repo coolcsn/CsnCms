0114285266bc63fb62d87cce8f872e198403d0ab
Svetoslav <mp5@mail.bg>
2013-09-16 17:34:28 +0300

    more data in db

--- data/SampleData.sql
+++ data/SampleData.sql
@@ -7,3 +7,10 @@ INSERT INTO `resource` (`id`, `name`) VALUES
 (2, 'Public Resource'),
 (3, 'Private Resource'),
 (4, 'Admin Resource');
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `name`) VALUES
+(1, 'Programming')
